e5731810bd899c02c2b3d15ddbfd04f0365a7fc8
João Marcos Viana <joaomarcosvianadias@gmail.com>
2026-06-10 14:26:46 -0300

    Revert "Back End"
    This reverts commit 87610ad9489dd3ba57ca88d71ee749bb28c6213f.

--- Back-End-Hikoki/src/main/java/com/testeSistemas/hikoki/Repository/DocClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testeSistemas.hikoki.Repository;
-
-import com.testeSistemas.hikoki.Entity.DocCliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocClienteRepository extends JpaRepository<DocCliente, Integer> {
-}
--- Back-End-Hikoki/src/main/java/com/testeSistemas/hikoki/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testeSistemas.hikoki.Repository;
-
-import com.testeSistemas.hikoki.Entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-}
